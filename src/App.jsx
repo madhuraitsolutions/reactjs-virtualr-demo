9059043c29c0a202f7449cc685bbede5622df00c
Kombiah <kombiahrk@yahoo.com>
2024-03-23 16:24:59 +0530

    Footer Component Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
@@ -15,6 +16,7 @@ export default function App() {
         <WorkflowSection/>
         <Pricing/>
         <Testimonials/>
+        <Footer/>
       </div>
     </>
   )
