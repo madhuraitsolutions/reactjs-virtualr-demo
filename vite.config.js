a8b1abeb446ddece18a2dcf0b9be01d7e53b7dad
Kombiah <kombiahrk@yahoo.com>
2024-04-30 16:03:12 +0530

    Vite Config updated to generate build version

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir:"./www/html/virtualrdemo/",
+    sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output:{
+        entryFileNames:"[name].js",
+        chunkFileNames:"[name].js",
+        assetFileNames:"[name].[ext]"
+      }
+    }
+  },
+  base: "/virtualrdemo/",
 })
